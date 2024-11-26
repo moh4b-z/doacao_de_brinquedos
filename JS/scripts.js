3a9239d31de69169189085e2cf586aa4a2a41011
moh4b-z <mohammadsalim10000@gmail.com>
2024-11-26 09:41:10 -0300

    processo de finalização

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,11 @@
 var chavePix = document.querySelector(".chavepix")
 var avisoCopiado = document.querySelector(".copiado")
 
-adicionarDataAtributo(chavePix, "chave", "deu certo")
+var entregas = document.querySelectorAll(".diaEntrega");
 
+
+
+// adicionarDataAtributo(chavePix, "chave", "deu certo")
 
 chavePix.addEventListener("click", function () {
     copiarDataAtributo(chavePix, "data-chave").then((copiado) => {
@@ -11,25 +14,47 @@ chavePix.addEventListener("click", function () {
             
             setTimeout(() => {
                 avisoCopiado.classList.remove("mostrar")
-            }, 1000)
+            }, 500)
         }
     })
 })
 
 
-// Função para copiar o valor
-function copiarDataAtributo(elemento, nomeAtributo) {
-    let valorAtributo = elemento.getAttribute(nomeAtributo)
-    
-    return navigator.clipboard.writeText(valorAtributo)
-        .then(() => true)
-        .catch((err) => {
-            console.error("Erro ao copiar:", err)
-            return false
-        })
-}
 
-// Função para criar e adicionar um data attribute
-function adicionarDataAtributo(elemento, nomeAtributo, valor) {
-    elemento.setAttribute(`data-${nomeAtributo}`, valor)
-}
+
+
+entregas.forEach(entrega => {
+    let video = entrega.querySelector(".video-player");
+    let source = video.querySelector("source");
+    let mensagem = entrega.querySelector(".mensagem");
+
+    // Verifica se o 'src' do vídeo está vazio
+    if (source.src) {
+        video.style.display = "block"; // Mostra o vídeo
+        mensagem.style.display = "none"; // Esconde a mensagem
+    } else {
+        video.style.display = "none"; // Esconde o vídeo
+        mensagem.style.display = "block"; // Mostra a mensagem
+    }
+});
+
+
+
+
+
+// // Função para copiar o valor
+// function copiarDataAtributo(elemento, nomeAtributo) {
+//     let valorAtributo = elemento.getAttribute(nomeAtributo)
+    
+//     return navigator.clipboard.writeText(valorAtributo)
+//         .then(() => true)
+//         .catch((err) => {
+//             console.error("Erro ao copiar:", err)
+//             return false
+//         })
+// }
+
+// // Função para criar e adicionar um data attribute
+// function adicionarDataAtributo(elemento, nomeAtributo, valor) {
+//     elemento.setAttribute(`data-${nomeAtributo}`, valor)
+// }
